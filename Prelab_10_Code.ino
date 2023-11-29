05e5c6e456251137b6acd081abdbb68db5052205
junexyin <152441823+junexyin@users.noreply.github.com>
2023-11-29 15:29:44 -0500

    Add files via upload

---
+++ Prelab_10_Code.ino
@@ -0,0 +1,41 @@
+// I worked alone to write this code
+
+const int PWMA = 11;
+const int AIN2 = 10;
+const int AIN1 = 9;
+const int STDBY = 9;
+const int BIN1 = 7; 
+const int BIN2 = 6;
+const int PWMB = 5;
+
+void setup() {
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(STDBY, OUTPUT);
+  digitalWrite(STDBY, HIGH);
+}
+
+void loop() {
+  drive(100,100); //drive forwards
+}
+
+void motorWrite(int spd, int pin_IN1, int pin_IN2, int pin_PWM){
+  if (spd < 0){ //go one way
+    digitalWrite(pin_IN1, HIGH); 
+    digitalWrite(pin_IN2, LOW);
+  }
+  else{ //go the other way
+    digitalWrite(pin_IN1, LOW);
+    digitalWrite(pin_IN2, HIGH);
+  }
+  analogWrite(pin_PWM, abs(spd));
+}
+
+void drive(int speedL, int speedR){
+  motorWrite(speedL, AIN1, AIN2, PWMA);
+  motorWrite(speedR, BIN1, BIN2, PWMB);
+}
